18b504764976ad2e853b38ee2e63af4bb8f753c4
Chris Zubak-Skees <chriszs@gmail.com>
2014-01-23 15:06:00 -0500

    Made visual "tests" somewhat more modular, WIP

--- test/test.js
+++ test/test.js
@@ -1,62 +1,88 @@
 (function () {
     var width = 900,
-        height = 600;
+        height = 600,
+        margin = 50;
 
-    var container = d3.select('.testContainer');
-
-    var svg = container.append('svg')
-                .attr('width',width)
-                .attr('height',height);
-
-    var g, text, wrap;
+    var tests = [];
+    
+    var container, svg;
 
     // test 1: simple text wraps at 100 pixels
 
-    g = svg.append('g')
-                    .attr('transform','translate(50 50)');
+    tests.push(function (x,y) {
+        var g = svg.append('g')
+                        .attr('transform','translate(' + x + ' ' + y + ')');
 
-    text = g.append('text')
-                    .attr('x',0)
-                    .attr('y',10)
-                    .text('This is some text, yo. It should wrap, yo.');
+        var text = g.append('text')
+                        .attr('x',0)
+                        .attr('y',10)
+                        .text('This is some text, yo. It should wrap and not get cut off, yo. This is some text, yo. It should wrap and not get cut off, yo.');
 
-    wrap = d3.svg.textWrap(text,100);
+        d3.svg.textWrap(text,100);
 
-    g.append('rect')
-        .attr({
-            'class': 'target bounds',
-            'x': 0,
-            'y': 0,
-            'width': 100,
-            'height': 100
-        });
+        g.append('rect')
+            .attr({
+                'class': 'target bounds',
+                'x': 0,
+                'y': 0,
+                'width': 100,
+                'height': '6em'
+            });
+    });
 
 
     // test 2: simple text wraps at 100 pixels, gets cut off if goes over height
 
-    g = svg.append('g')
-                    .attr('transform','translate(250 50)');
+    tests.push(function (x,y) {
 
-    text = g.append('text')
-                    .attr('x',0)
-                    .attr('y',10)
-                    .text('This is some text, yo. It should wrap and get cut off, yo. This is some text, yo. It should wrap and get cut off, yo.');
+        var g = svg.append('g')
+                        .attr('transform','translate(' + x + ' ' + y + ')');
 
-    wrap = d3.svg.textWrap(text,100)
-                    .height(6); // height is in ems
+        var text = g.append('text')
+                        .attr('x',0)
+                        .attr('y',10)
+                        .text('This is some text, yo. It should wrap and get cut off, yo. This is some text, yo. It should wrap and get cut off, yo.');
 
-    if (wrap.overflow() === false) {
-        console.error('test 2: wrap overflow should not equal false');
+        var wrap = d3.svg.textWrap(text,100)
+                        .height(6); // height is in ems
+
+        if (wrap.overflow() === false) {
+            console.error('test 2: wrap overflow should not equal false');
+        }
+
+        g.append('rect')
+            .attr({
+                'class': 'target bounds',
+                'x': 0,
+                'y': 0,
+                'width': 100,
+                'height': '6em'
+            });
+    });
+
+
+    function init () {
+        container = d3.select('.testContainer');
+
+        svg = container.append('svg')
+                    .attr('width',width)
+                    .attr('height',height);
+
+        var curX = margin,
+            curY = margin;
+
+        tests.forEach(function (test) {
+            test(curX,curY);
+
+            curX += 200;
+
+            if (curX > width-margin) {
+                curY += 200;
+                curX = margin;
+            }
+        });
     }
 
-    g.append('rect')
-        .attr({
-            'class': 'target bounds',
-            'x': 0,
-            'y': 0,
-            'width': 100,
-            'height': '6em'
-        });
-
+    init();
 
 })();
